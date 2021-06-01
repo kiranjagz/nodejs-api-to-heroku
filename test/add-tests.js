aa4bfe5ed95de490b736e22690563fa9e90129b7
Kiran Jagathlal <kiranjagz@gmail.com>
2021-06-01 20:36:49 +0200

    Change using chai should

--- test/add-tests.js
+++ test/add-tests.js
@@ -1,9 +1,11 @@
-const { expect } = require('chai');
+const chai = require('chai');
 const addService = require('./../services/add-service');
+
+chai.should();
 
 describe("add two positive numbers: 2 + 2", () => {
     it('should return 4', function() {
         var result = addService.add(2, 2);
-        expect(result).to.equal(4);
+        result.should.equal(4);
     });
 });
